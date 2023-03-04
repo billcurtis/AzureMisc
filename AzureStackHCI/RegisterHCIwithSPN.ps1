ad282fda0c0d1f436c2316e871cdac5aaab79778
wcurtis <wcurtis@microsoft.com>
2023-03-04 09:16:51 -0500

    intial-commit

---
+++ AzureStackHCI/RegisterHCIwithSPN.ps1
@@ -0,0 +1,42 @@
+# static variables
+
+$subscriptionID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+$tenantID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+$clientID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+$clientSecret =  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+$resourcegroupName = 'azstack-rg'
+$arcserverresourceGroupName = 'azstack-rg'
+
+# preferences
+
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'SilentlyContinue'
+
+
+# import modules
+
+Import-Module Az.StackHCI
+$VerbosePreference = 'Continue'
+
+
+# create credential for service principal
+$arcSpnCredential = new-object -typename System.Management.Automation.PSCredential `
+        -argumentlist (($clientID),(ConvertTo-SecureString $clientSecret -AsPlainText -Force))
+
+
+
+# Ensure that the SP is OWNER of the RG
+# Perms for SP = "Azure Stack HCI registration role"
+
+Connect-AzAccount -ServicePrincipal -Credential $arcSpnCredential -Tenant $tenantID
+
+# register
+ $accessToken = Get-AzAccessToken 
+ Register-AzStackHCI `
+ -ResourceGroupName $resourcegroupName `
+ -ArcServerResourceGroupName $arcserverresourceGroupName `
+  -SubscriptionId $subscriptionID `
+  -ArmAccessToken $accessToken.Token `
+  -AccountId $accessToken.UserId `
+  -ArcSpnCredential $arcSpnCredential `
+  -Confirm:$false
